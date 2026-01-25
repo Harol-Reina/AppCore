244f066c6024eae834c093c26c6114f3768a0e05
Harol A. Reina H. <hreina@tuprimero.com.co>
2026-01-24 21:42:41 -0500

    refactor: simplify NotFoundException by leveraging HttpBaseException inheritance
    - Remove redundant MessageLog property (inherited from CustomException)
    - Remove custom ToString() implementation (inherited behavior)
    - Simplify constructor to delegate to HttpBaseException with status code 404
    - Maintain same public API while reducing code duplication
    - Improve exception hierarchy consistency

--- src/AppCore/Application/Exceptions/NotFoundException.cs
+++ src/AppCore/Application/Exceptions/NotFoundException.cs
@@ -1,23 +1,11 @@
-using System.Runtime.CompilerServices;
-using AppCore.Application.Wrappers;
+﻿using System.Runtime.CompilerServices;
 
 namespace AppCore.Application.Exceptions;
 
 public class NotFoundException : HttpBaseException {
-    public MessageLog MessageLog { get; }
     public NotFoundException(string message = "The requested resource could not be found.",
                              [CallerMemberName] string memberName = "",
                              [CallerFilePath] string sourceFilePath = "",
-                             [CallerLineNumber] int sourceLineNumber = 0) : base(message, 404) {
-         MessageLog = new MessageLog {
-            Tipo = base.GetType().Name,
-            Source = base.Source,
-            Message = message,
-            Metodo = memberName,
-            Path = $"{sourceFilePath} Line: {sourceLineNumber}",
-        };
+                             [CallerLineNumber] int sourceLineNumber = 0) : base(message, 404, memberName, sourceFilePath, sourceLineNumber) {
     }
-
-    public override string ToString()
-        => MessageLog.ToString();
 }
