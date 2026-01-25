b407b77803ea8b43f798f6a13b9425dad1ae7bc4
Harol A. Reina H. <hreina@tuprimero.com.co>
2026-01-25 12:07:39 -0500

    test: add exception tests (ApiHttpException, HttpBaseException) - coverage 65.7% → 67.4%
    - Add ApiHttpExceptionTests (4 tests) for HTTP connection exceptions
    - Add HttpBaseExceptionTests (5 tests) with Theory for status codes
    - Tests: 374 passed (+11), 2 skipped
    - Coverage: Line 67.4% (+1.7%), Branch 63.1%
    - Total tests: 399 (374 unit + 25 SpecFlow)

---
+++ tests/AppCore.UnitTests/Application/Exceptions/ApiHttpExceptionTests.cs
@@ -0,0 +1,82 @@
+﻿using AppCore.Application.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace AppCore.UnitTests.Application.Exceptions;
+
+public class ApiHttpExceptionTests
+{
+    [Fact]
+    public void Constructor_WithSimpleException_ShouldNotThrow()
+    {
+        // Arrange & Act
+        var act = () =>
+        {
+            try
+            {
+                throw new Exception("Connection failed");
+            }
+            catch (Exception ex)
+            {
+                return new ApiHttpException(ex);
+            }
+        };
+
+        // Assert
+        act.Should().NotThrow();
+    }
+
+    [Fact]
+    public void Constructor_ShouldSetCorrectMessage()
+    {
+        // Arrange & Act
+        try
+        {
+            throw new InvalidOperationException("Test error");
+        }
+        catch (Exception ex)
+        {
+            var exception = new ApiHttpException(ex);
+
+            // Assert
+            exception.Message.Should().Be("The connection to the requested URL cannot be made.");
+            exception.MessageLog.Should().NotBeNull();
+        }
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnMessageLogString()
+    {
+        // Arrange & Act
+        try
+        {
+            throw new Exception("Test");
+        }
+        catch (Exception ex)
+        {
+            var exception = new ApiHttpException(ex);
+            var result = exception.ToString();
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+        }
+    }
+
+    [Fact]
+    public void MessageLog_ShouldContainExceptionType()
+    {
+        // Arrange & Act
+        try
+        {
+            throw new ArgumentNullException("param", "Parameter cannot be null");
+        }
+        catch (Exception ex)
+        {
+            var exception = new ApiHttpException(ex, "MyMethod");
+
+            // Assert
+            exception.MessageLog.Tipo.Should().Be("ApiHttpException");
+            exception.MessageLog.Metodo.Should().Be("MyMethod");
+        }
+    }
+}
---
+++ tests/AppCore.UnitTests/Application/Exceptions/HttpBaseExceptionTests.cs
@@ -0,0 +1,67 @@
+﻿using AppCore.Application.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace AppCore.UnitTests.Application.Exceptions;
+
+public class HttpBaseExceptionTests
+{
+    [Fact]
+    public void Constructor_WithStatusCode_ShouldSetStatusCode()
+    {
+        // Arrange & Act
+        var exception = new HttpBaseException("Not found", 404);
+
+        // Assert
+        exception.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public void Constructor_ShouldInheritFromCustomException()
+    {
+        // Arrange & Act
+        var exception = new HttpBaseException("Server error", 500);
+
+        // Assert
+        exception.Should().BeAssignableTo<CustomException>();
+    }
+
+    [Fact]
+    public void Constructor_ShouldCreateDictionaryErrorWithStatusCode()
+    {
+        // Arrange & Act
+        var exception = new HttpBaseException("Unauthorized", 401, "TestMethod", "/path/test.cs", 42);
+
+        // Assert
+        exception.StatusCode.Should().Be(401);
+        exception.MessageLog.Should().NotBeNull();
+        exception.MessageLog.Message.Should().BeOfType<DictionaryError>();
+    }
+
+    [Fact]
+    public void Constructor_WithCallerInfo_ShouldPopulateMessageLog()
+    {
+        // Arrange & Act
+        var exception = new HttpBaseException("Bad request", 400, "MyMethod", "/src/file.cs", 10);
+
+        // Assert
+        var error = exception.MessageLog.Message as DictionaryError;
+        error.Should().NotBeNull();
+        error!.Code.Should().Be("HTTP-400");
+        error.Message.Should().Be("Bad request");
+    }
+
+    [Theory]
+    [InlineData(200, "HTTP-200")]
+    [InlineData(404, "HTTP-404")]
+    [InlineData(500, "HTTP-500")]
+    public void Constructor_WithDifferentStatusCodes_ShouldCreateCorrectErrorCode(int statusCode, string expectedCode)
+    {
+        // Arrange & Act
+        var exception = new HttpBaseException("Test", statusCode);
+
+        // Assert
+        var error = exception.MessageLog.Message as DictionaryError;
+        error!.Code.Should().Be(expectedCode);
+    }
+}
