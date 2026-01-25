f2fbc96ee401b6cbd854f8b0b40c117514e54adc
Harol A. Reina H. <hreina@tuprimero.com.co>
2026-01-24 21:42:28 -0500

    refactor: migrate HttpBaseException to inherit from CustomException
    - Change HttpBaseException to inherit from CustomException instead of Exception
    - Add proper constructor with caller information parameters
    - Generate error code dynamically using HTTP status code (HTTP-{statusCode})
    - Maintain StatusCode property for HTTP-specific functionality
    - Improve exception hierarchy consistency

--- src/AppCore/Application/Exceptions/HttpBaseException.cs
+++ src/AppCore/Application/Exceptions/HttpBaseException.cs
@@ -1,5 +1,16 @@
+﻿using AppCore.Application.Wrappers;
+using System.Runtime.CompilerServices;
+
 namespace AppCore.Application.Exceptions;
 
-public class HttpBaseException(string message, int statusCode) : Exception(message) {
-    public int StatusCode { get; } = statusCode;
+public class HttpBaseException : CustomException {
+    public int StatusCode { get; }
+    
+    public HttpBaseException(string message, int statusCode,
+                           [CallerMemberName] string memberName = "",
+                           [CallerFilePath] string sourceFilePath = "",
+                           [CallerLineNumber] int sourceLineNumber = 0) 
+        : base(new DictionaryError("HTTP-" + statusCode.ToString(), message), memberName, sourceFilePath, sourceLineNumber) {
+        StatusCode = statusCode;
+    }
 }
