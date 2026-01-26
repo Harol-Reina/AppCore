ae8cd5d21bcd867d3e5dd3d1aa9121ea3dc57df4
Harol A. Reina H. <hreina@tuprimero.com.co>
2026-01-26 18:17:42 -0500

    refactor(core): promote AuditableExtensions to AppCore logic

---
+++ src/AppCore/Infrastructure/Extensions/AuditableExtensions.cs
@@ -0,0 +1,18 @@
+using AppCore.Application.Interfaces;
+using AppCore.Infrastructure.Data.DAOs.Common;
+
+namespace AppCore.Infrastructure.Extensions;
+
+public static class AuditableExtensions {
+    public static void SetAuditCreate(this AuditableBaseDao entity, ICurrentUserService userService) {
+        entity.CreatedAt = DateTime.UtcNow;
+        entity.CreatedBy = userService.GetUserName(); 
+        entity.UpdatedAt = null;
+        entity.UpdatedBy = null;
+    }
+
+    public static void SetAuditUpdate(this AuditableBaseDao entity, ICurrentUserService userService) {
+        entity.UpdatedAt = DateTime.UtcNow;
+        entity.UpdatedBy = userService.GetUserName();
+    }
+}
